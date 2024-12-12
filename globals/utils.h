0e88829e0ab774a4d8dda42b4fe4fc7cb778e76a
dlazder <dlazder51@gmail.com>
2024-12-12 16:34:46 +0300

    move utils imports to another file

---
+++ globals/utils.h
@@ -0,0 +1,10 @@
+// 1st layer utilities
+#include "../utils/btnUtils.h"
+#include "../utils/displayUtils.h"
+#include "../utils/timerUtils.h"
+#include "../utils/webServerUtils.h"
+#include "../utils/webControlUtils.h"
+#include "../utils/webResponseUtils.h"
+// 2nd layer utilities
+#include "../utils/procManagmentUtils.h"
+#include "../utils/menuUtils.h"
--- m5stick_power_firmware.ino
+++ m5stick_power_firmware.ino
@@ -1,14 +1,5 @@
 #include "./globals/globals.h"
-// 1st layer utilities
-#include "./utils/btnUtils.h"
-#include "./utils/displayUtils.h"
-#include "./utils/timerUtils.h"
-#include "./utils/webServerUtils.h"
-#include "./utils/webControlUtils.h"
-#include "./utils/webResponseUtils.h"
-// 2nd layer utilities
-#include "./utils/procManagmentUtils.h"
-#include "./utils/menuUtils.h"
+#include "./globals/utils.h"
 
 #include "./globals/functions.h"
 
