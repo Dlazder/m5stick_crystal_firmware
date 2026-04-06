7698ee5c9ee824a5b1247dd92fca1a4fe645f8f5
Dlazder <dlazder51@gmail.com>
2026-04-06 23:34:23 +0300

    fix: clock size increased, flickering resolved

--- functions/clockLoop.h
+++ functions/clockLoop.h
@@ -1,17 +1,20 @@
 // pid 1
 
-int oldSeconds;
 void clockLoop() {
+  static int oldSeconds;
   auto dt = StickCP2.Rtc.getDateTime();
   char formatString[30];
   sprintf(formatString, "%02d:%02d:%02d", dt.time.hours, dt.time.minutes, dt.time.seconds);
   if (dt.time.seconds != oldSeconds) {
     oldSeconds = dt.time.seconds;
     DISP.setTextColor(FGCOLOR, BGCOLOR);
-    clearScreenWithSymbols();
-    centeredPrint(formatString, SMALL_TEXT);
+    canvas.clear();
+    canvas.setTextSize(MEDIUM_TEXT);
+    canvas.drawCenterString(formatString, DISP.width() / 2, ((DISP.height() - getStatusBarHeight()) - DISP.fontHeight()) / 2);
+    canvas.pushSprite(0, getStatusBarHeight() + 1);
   }
-  checkExit(0);
+  checkExit();
+  
   if (isWebDataRequested()) {
     webData = generateWebData("function", generateFunctionElement(formatString, SMALL_TEXT, "center"));
   }
